371fad202bc365803d83d3fcba2803374b9b3925
Kartik Buttan <2021btechaidskartik9829@poornima.edu.in>
2024-09-15 19:56:29 +0530

    added new Contributor

---
+++ profiles/Kartik_Buttan.md
@@ -0,0 +1,18 @@
+# Your Name
+Kartik Buttan
+
+### Location
+
+Jaipur, India 
+
+### Academics
+
+Poornima University, Jaipur
+
+### Interests
+
+- Swimming, Gaming, Coding
+
+### Profile Link
+
+[Kartik Buttan](https://github.com/kartik1112)
---
+++ scripts/hello_kartik.go
@@ -0,0 +1,12 @@
+// LANGUAGE: GoLang
+// AUTHOR: Kartik Buttan
+// GITHUB: https://github.com/kartik1112
+
+package main
+
+import "fmt"
+
+func main() {
+     fmt.Println("Hello, World!");
+     fmt.Println("Super Excited for HACKTOBERFEST");
+}
